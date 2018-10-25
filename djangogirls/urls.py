f37a8de4a83606c5124f08abd85c575bbe5e34f7
hunt4499 <ajsood160@gmail.com>
2018-10-25 22:21:52 +0530

    Login functionality added /changes in settings and login template added

--- djangogirls/urls.py
+++ djangogirls/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf.urls import include,url
 
 urlpatterns = [
     path('',include('blog.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
